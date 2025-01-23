2e8aaee21f1e323264d2d973e6634bcd75fab3cf
Marina Lasorsa <marina.lasorsa@gmail.com>
2025-01-23 18:33:30 +0100

    ADD: created migrations for companies and employees tables

---
+++ database/migrations/2025_01_23_153111_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('set null');
+            $table->string('name', 200);
+            $table->string('surname', 200);
+            $table->string('telephone', 20);
+            $table->string('email', 320)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
